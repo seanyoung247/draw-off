aec5a062a1c023c9436bd06c5cbbb65edd4e95d6
Sean Young <seanyoung247@gmail.com>
2022-05-24 09:24:20 +0100

    feat: Adds inital test server

--- server/app.py
+++ server/app.py
@@ -1,7 +1,11 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+@app.route('/time')
+def get_time():
+    return { "time": time.time() }
+
+if __name__ == "__main__":
+    app.run(debug=True)
